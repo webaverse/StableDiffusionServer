5a9784cc026f06b9eb78cc41c00190ab3dda7af9
Avaer Kazmer <a@modules.io>
2022-11-03 20:13:12 -0400

    Update invoke_ai_web_server.py

--- invoke_ai_web_server.py
+++ invoke_ai_web_server.py
@@ -99,8 +99,8 @@ class InvokeAIWebServer:
                 "seamless": args.get("seamless", default=False),
                 "init_img": args.get("init_img", default=None), # For img2img
                 "init_mask": args.get("init_mask", default=None), # For inpainting
-                "strength": args.get("strength", default=0.75),
-                "inpaint_replace": args.get("inpaint_replace", default=0.0),
+                "strength": float(args.get("strength", default=0.75)),
+                "inpaint_replace": float(args.get("inpaint_replace", default=0.0)),
             }
             # print the computed prompt
             print("prompt: ", arg_dict["prompt"])
